5e47b1ed2cbe44b42fd46a48eef7ceb6e5ecd534
Benjamin Sautner <bsautner@gmail.com>
2025-01-09 16:31:41 -0500

    compose wasm working

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,12 +1,7 @@
-import org.jetbrains.kotlin.gradle.internal.platform.wasm.WasmPlatforms.wasmJs
-import org.gradle.internal.impldep.com.google.api.services.storage.Storage
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.dsl.*
-import org.jetbrains.kotlin.gradle.internal.Kapt3GradleSubplugin.Companion.disableClassloaderCacheForProcessors
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
 plugins {
-    alias(libs.plugins.buildsrc)
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
@@ -22,75 +17,64 @@ ksp {
 }
 
 kotlin {
-   
 
-  sourceSets {
-        jvm()
-//
-//      @OptIn(ExperimentalWasmDsl::class)
-//      wasmJs {
-//          moduleName = "kobold-sample-app"
-//          browser {
-//              val rootDirPath = project.rootDir.path
-//              val projectDirPath = project.projectDir.path
-//              commonWebpackConfig {
-//                  outputFileName = "composeApp.js"
-//                  devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-//                      static = (static ?: mutableListOf()).apply {
-//                          // Serve sources to debug inside browser
-//                          add(rootDirPath)
-//                          add(projectDirPath)
-//                      }
-//                  }
-//              }
-//          }
-//          binaries.executable()
-//      }
+    jvm() {}
 
-      commonMain {}
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        moduleName = "kobold-sample-app"
+        binaries.executable()
+        browser {
+            val rootDirPath = project.rootDir.path
+            val projectDirPath = project.projectDir.path
+            commonWebpackConfig {
+                outputFileName = "composeApp.js"
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf()).apply {
+                        add(rootDirPath)
+                        add(projectDirPath)
+                    }
+                }
+            }
+        }
+    }
 
-      val jvmMain by getting {
-          dependencies {
+    sourceSets {
+        val jvmMain by getting {}
 
-          }
-      }
+        val commonMain by getting {
+            kotlin.srcDir("src/commonMain/kotlin")
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                implementation(compose.ui)
+                implementation(compose.components.resources)
+                implementation(compose.components.uiToolingPreview)
+                implementation(libs.androidx.lifecycle.viewmodel)
+                implementation(libs.androidx.lifecycle.runtime.compose)
 
-      val commonMain by getting {
-          kotlin.srcDir("src/commonMain/kotlin")
-          dependencies {
-             // implementation(libs.ksp)
-              implementation(compose.runtime)
-              implementation(compose.foundation)
-              implementation(compose.runtime)
-              implementation(compose.foundation)
-              implementation(compose.material)
-              implementation(compose.ui)
-              implementation(compose.components.resources)
-              implementation(compose.components.uiToolingPreview)
-              implementation(libs.androidx.lifecycle.viewmodel)
-              implementation(libs.androidx.arch.core.common)
-              implementation(libs.androidx.lifecycle.runtime.compose)
-              implementation(libs.androidx.annotation)
+            }
+        }
 
+        val wasmJsMain by getting {
+            dependencies {
+                // Added missing dependency for Compose Web
 
-
-          }
-      }
-
-
-  }
+            }
+        }
+    }
 }
+
 dependencies {
     kspCommonMainMetadata(project(":ksp"))
-
-    add("kspCommonMainMetadata", project(":ksp"))
-    add("kspJvm", project(":ksp"))
+    add("kspWasmJs", project(":ksp"))
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+    compilerOptions {
+        freeCompilerArgs.add("-Xjsr305=strict")
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
